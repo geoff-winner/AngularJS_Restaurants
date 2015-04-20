68bb0dff84f4c36eaee3ab8b2186939009975b43
geoff winner & marc lee <student@epicodus.com>
2015-04-20 12:02:00 -0700

    add price and location fields, searchable

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 bestRestaurant.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
   $scope.restaurants = [];
   $scope.addRestaurant = function() {
-    $scope.restaurants.push({ name: $scope.restaurantName, type: $scope.restaurantType });
+    $scope.restaurants.push({ name: $scope.restaurantName, type: $scope.restaurantType, price: $scope.restaurantPrice, location: $scope.restaurantLocation });
     $scope.restaurantType = null;
     $scope.restaurantName = null;
+    $scope.restaurantPrice = null;
+    $scope.restaurantLocation = null;
   };
   $scope.deleteRestaurant = function(restaurant) {
     var index = $scope.restaurants.indexOf(restaurant)
